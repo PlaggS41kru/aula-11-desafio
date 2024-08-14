8196a70655a3346ff2bfe5103094a570a96aabc1
PlaggS41kru <neris.gilson@hotmail.com>
2024-08-14 10:05:22 -0300

    Add files via upload

--- code/script.js
+++ code/script.js
@@ -1,1 +1,23 @@
+const listaDeTarefas = [];
+const tarefaInput = document.getElementById('taskInput').Value;
+const botao = document.getElementById('addTaskButton')
+
+function adicionarTarefa(tarefa){
+    listaDeTarefas.push(tarefa);
+}
+
+botao.addEventListener('click', ()=>{
+    const tarefa = document.getElementById('taskInput').value;
+    adicionarTarefa(tarefa);
+});
+
+let addEventListener = listaDeTarefas.length;;
+
+let text  = '<ul>';
+for (let i = 0; i < adicionarTarefa; i++){
+    text += '<li>${listaDeTarefas[i]}</li>';
+}
+
+text += '</ul>';
+
 
